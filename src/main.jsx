1af69d16119b1e7d3bae7e0c0cb854de79c6c31c
murtaza <murtazakhanghori@gmail.com>
2024-12-14 14:19:20 -0800

    update

---
+++ src/main.jsx
@@ -0,0 +1,21 @@
+import React from 'react';  // Use single or double quotes
+import ReactDOM from 'react-dom';  // Correct spelling and quotes
+
+const Rootdiv = document.getElementById('root');  // Single quotes for the ID
+
+ReactDOM.createRoot(Rootdiv).render(
+  <div>
+    <h1>React js</h1>
+    <h1>Second Practice</h1>
+    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit.<br/> Mollitia, necessitatibus aliquam totam quae ipsam sed,<br/> voluptatum ipsum est eum, ullam dignissimos explicabo voluptates <br/> et sapiente? Earum aliquid neque a error.</p>
+    <img src="/vite.svg" alt="Vite Logo" />
+    
+    <p>
+  Lorem ipsum dolor sit amet, consectetur adipiscing elit.<br />
+  Vivamus lacinia odio vitae vestibulum.<br />
+  Cras venenatis euismod malesuada.
+</p>
+<button>submit</button>
+
+  </div>
+);
